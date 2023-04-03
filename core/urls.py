cda0e836eed5af9b18e81084418cd951c33f8a02
spirostrakadas <111435322+spirostrakadas@users.noreply.github.com>
2023-04-03 17:18:38 +0300

    finished signup

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('',views.index,name='home'),
-    path('/<str:pk>/',views.detail,name='detail')
+    path('post/<str:pk>/',views.detail,name='detail'),
+    path('signup',views.signup,name='signup')
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render,get_object_or_404
-from.models import Post,User,Profile
+from django.shortcuts import render,get_object_or_404,redirect
+from.models import Post,Profile
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model, login, authenticate
+from django.contrib import messages
 
 
 # Create your views here.
+User = get_user_model()
+
+
+
 def index(request):
-    user_object=User.objects.get(username=request.user.username)
+    user_object=User.objects.get(username=request.user)
     user_profile=Profile.objects.get(user=user_object)
     post=Post.objects.all()
     return render(request,'index.html',{'post':post,'user_profile':user_profile})
@@ -14,3 +21,21 @@ def index(request):
 def detail(request,pk):
     post=get_object_or_404(Post,id=pk)
     return render(request,'detail.html',{'post':post})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = True
+            user.save()
+            # create a Profile instance for the new user
+            profile = Profile(user=user)
+            profile.save()
+            # log the user in
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+    
